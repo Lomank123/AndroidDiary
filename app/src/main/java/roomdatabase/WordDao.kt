461925a71e0ff89a4a285f2013badb0f6582f673
Lomank <lomank20022@gmail.com>
2020-03-08 01:09:18 +0500

    Revert "change"
    This reverts commit eeaca26565d4c9cd9aa935959f72721a4dfad65b.

--- app/src/main/java/roomdatabase/WordDao.kt
+++ app/src/main/java/roomdatabase/WordDao.kt
@@ -59,7 +59,6 @@ interface WordDao {
 
    // @Query("SELECT * from note_table WHERE diaryId =:ID")
    // fun getNeededNotes(ID : Long) : LiveData<List<Note>>
-
    // обновляет заметку
     @Update
     suspend fun updateNote(note : Note)
