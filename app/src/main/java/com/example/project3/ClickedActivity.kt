274ebebb445f33d7e7325560c0d8764d86bf97c3
Lomank <lomank20022@gmail.com>
2020-03-11 08:46:12 +0500

    Добавлено меню для ClickedActivity с кнопками Save и Cancel сверху

--- app/src/main/java/com/example/project3/ClickedActivity.kt
+++ app/src/main/java/com/example/project3/ClickedActivity.kt
@@ -4,19 +4,24 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_clicked.*
 
 class ClickedActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clicked)
 
+        val noteId = intent.getLongExtra("note_idNote", -1)
+
         // получаем экстра данные из NoteActivity
         textView1.text = intent.getStringExtra("note_name")
         editText1.setText(intent.getStringExtra("note_text"))
-
-        val noteId = intent.getLongExtra("note_idNote", -1)
 
         // Обработчик нажатий для кнопки Save
         button_save1.setOnClickListener {
@@ -43,6 +48,52 @@ class ClickedActivity : AppCompatActivity() {
             setResult(Activity.RESULT_CANCELED)
             finish()
         }
+    }
+
+    // создает OptionsMenu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_edit_note, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // когда выбираешь элемент меню
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.delete_edit -> {
+                // удаление записи
+                Toast.makeText(this, "Delete note", Toast.LENGTH_SHORT).show()
+                return super.onOptionsItemSelected(item)
+            }
+            R.id.open_edit -> {
+                // открытие записи
+                Toast.makeText(this, "Open note", Toast.LENGTH_SHORT).show()
+                return super.onOptionsItemSelected(item)
+            }
+            R.id.save_btn_edit -> { // Кнопка Save
+                val noteId = intent.getLongExtra("note_idNote", -1)
+                val replyIntent = Intent()
+
+                // создаем массив с названием и текстом заметки
+                val note = arrayListOf(textView1.text.toString(),
+                    editText1.text.toString())
+
+                // кладем то, что записано в массив и передаем по тегу EXTRA_REPLY_EDIT
+                replyIntent.putExtra(EXTRA_REPLY_EDIT, note)
+                // также передаем idNote заметки чтобы обновить ее в NoteActivity
+                replyIntent.putExtra("noteId", noteId)
+
+                setResult(Activity.RESULT_OK, replyIntent) // resultCode будет RESULT_OK
+
+                // Завершаем работу с активити
+                Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            R.id.cancel_btn_edit -> { // Кнопка Cancel
+                setResult(Activity.RESULT_CANCELED)
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     // тег для распознавания именно этого запроса
