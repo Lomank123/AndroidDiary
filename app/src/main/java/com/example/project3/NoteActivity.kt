e38d0e878e804c5541594a31909a5708a5f5fd6d
Lomank <lomank20022@gmail.com>
2020-03-25 15:18:07 +0500

    1

--- app/src/main/java/com/example/project3/NoteActivity.kt
+++ app/src/main/java/com/example/project3/NoteActivity.kt
@@ -101,6 +101,11 @@ class NoteActivity : AppCompatActivity() {
         //    closeFabMenu()
         //    Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show()
         //}
+        // обработчик нажатий на 1-ую кнопку (пока ничего не делает)
+        fab1.setOnClickListener {
+            closeFabMenu()
+            Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show()
+        }
 
         // обработчик нажатий на 2-ую кнопку (вызывает NewWordActivity для создания заметки)
         fab2.setOnClickListener {
@@ -123,11 +128,11 @@ class NoteActivity : AppCompatActivity() {
         // возвращает элементы на исходные позиции
         fab.animate().rotation(0f)
         bg_fab_menu.animate().alpha(0f)
-        //fab1.animate().translationY(0f).rotation(90f)
+        fab1.animate().translationY(0f).rotation(90f)
         fab2.animate().translationY(0f).rotation(90f)
 
         // ставит задержку на исчезновение элементов меню (250 мс)
-        //Handler().postDelayed({fab1.visibility = GONE}, 250)
+        Handler().postDelayed({fab1.visibility = GONE}, 250)
         Handler().postDelayed({fab2.visibility = GONE}, 250)
         Handler().postDelayed({bg_fab_menu.visibility = GONE}, 250)
     }
@@ -137,14 +142,14 @@ class NoteActivity : AppCompatActivity() {
         isFabOpen = true
 
         // показывает элементы
-        //fab1.visibility = VISIBLE
+        fab1.visibility = VISIBLE
         fab2.visibility = VISIBLE
         bg_fab_menu.visibility = VISIBLE
 
         // "выдвигает" элементы
         fab.animate().rotation(135f)
         bg_fab_menu.animate().alpha(1f)
-        //fab1.animate().translationY(-300f).rotation(0f)
+        fab1.animate().translationY(-300f).rotation(0f)
         fab2.animate().translationY(-165f).rotation(0f)
     }
 
