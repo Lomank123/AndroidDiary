9fa62786cc0ac7bfece3934e68b54a644a8dd06e
Lomank <lomank20022@gmail.com>
2020-09-27 22:17:23 +0500

    Исправлена ошибка, связанная с преждевременным окончанием работы приложения и аудио-заметкой

--- app/src/main/java/com/lomank/diary/SplashActivity.kt
+++ app/src/main/java/com/lomank/diary/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import java.io.File
 
 // Экран загрузки приложения
 class SplashActivity : AppCompatActivity() {
@@ -20,5 +21,12 @@ class SplashActivity : AppCompatActivity() {
             finish()
 
         }, 300)
+    }
+    // this helps to delete empty voice note in new note if app was closed unexpectedly
+    override fun onDestroy() {
+        super.onDestroy()
+        val name = this.getExternalFilesDir(null)!!.absolutePath + "/voice_note_empty.3gpp"
+        if(File(name).exists())
+            File(name).delete()
     }
 }
